6dc3e2bd43c8f96d81716ad7c0682e5a8db27836
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2024-01-17 13:47:08 -0500

    fix region indexing (by blocks, by row/col)

--- tests/load_map.cpp
+++ tests/load_map.cpp
@@ -7,9 +7,11 @@
 int main() {
     std::unique_ptr<bwmodel::Map> map =
         bwmodel::Map::load_from("./data/example.bwmap");
-    assert(map->regions_at(0, 2)
+    assert(map->regions_at(0, 2 * bwmodel::REGION_H)
            == (bwmodel::RegionSet::PINK | bwmodel::RegionSet::BASE));
-    assert(map->regions_at(4, 2) == (bwmodel::RegionSet::MIDDLE));
-    assert(map->regions_at(7, 7) == (bwmodel::RegionSet::DIAMOND));
+    assert(map->regions_at(4 * bwmodel::REGION_W, 2 * bwmodel::REGION_H)
+           == (bwmodel::RegionSet::MIDDLE));
+    assert(map->regions_at(7 * bwmodel::REGION_W, 7 * bwmodel::REGION_H)
+           == (bwmodel::RegionSet::DIAMOND));
     return 0;
 }
